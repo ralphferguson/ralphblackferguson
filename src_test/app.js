0e2bd6f2aa621aa9d4f20d6f81d64b5046ca1698
ralphferguson <ralph.blk@gmail.com>
2018-11-10 14:01:55 +0100

    Add files via upload

---
+++ src_test/app.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+class Accordion extends React.Component {
+
+  state = {
+    currentIndex: -1
+  };
+
+  handleChange= i => {
+    this.setState({
+      currentIndex:i
+    })
+  };
+
+  render() {
+    const { handleChange } = this;
+    const { currentIndex } = this.state;
+
+
+    return (
+      <div>
+      {qa.map (function(e,i) {
+        return (<List
+        question={e.question}
+        answer={e.answer}
+        handleChange={handleChange}
+        key={i}
+        index = {i}
+        currentIndex={currentIndex}
+        />);
+      })}
+
+      </div>
+    );
+  }
+}
+
+export default Accordion;
