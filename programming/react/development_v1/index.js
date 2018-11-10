c8b43ddad9c46e73b427b74e64c8fbdf8907590c
ralphferguson <ralph.blk@gmail.com>
2018-11-10 14:07:36 +0100

    Add files via upload

---
+++ programming/react/development_v1/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Accordion from './app';
+
+function Helloworld() {
+  return <h1>Hello World</h1>
+}
+
+ReactDOM.render(<Accordion/>,document.getElementById('root'))
