2335a315f3cb7139aa8baf35d648f8a9ca2683fb
ralphferguson <ralph.blk@gmail.com>
2019-02-13 10:52:45 +0100

    Add files via upload

---
+++ programming/connectitude/design_script.js
@@ -0,0 +1,3 @@
+function replaceCss(css) {
+  document.getElementById('pagestyle').setAttribute('href', css)
+}
---
+++ programming/connectitude/precache-manifest.1a73d9c53e8e8bd0c76d435b2cd6123d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77f6e558a7d3e83b5e3fe4561b598fa4",
+    "url": "/connectitude/static/media/asset_chart.77f6e558.png"
+  },
+  {
+    "revision": "5c5e57b15f45a432a0c6",
+    "url": "/connectitude/static/js/runtime~main.5c5e57b1.js"
+  },
+  {
+    "revision": "d81124f4fdf381094047",
+    "url": "/connectitude/static/js/main.d81124f4.chunk.js"
+  },
+  {
+    "revision": "c789fb6ef3d9f84c403d",
+    "url": "/connectitude/static/js/1.c789fb6e.chunk.js"
+  },
+  {
+    "revision": "db3867197f40644f719a76b8ebd3deee",
+    "url": "/connectitude/index.html"
+  }
+];
